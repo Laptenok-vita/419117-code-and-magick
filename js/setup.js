d2daeb3ea7a02ed80182f841ae0e23e0250fb421
Laptenok-vita <sanaeva.va@yandex.ru>
2018-06-09 11:26:21 +0300

    Делает создание массива функцией, добавляет эту функцию в цикл

--- js/setup.js
+++ js/setup.js
@@ -8,28 +8,17 @@ var COATS_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161
 
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var wizards = [
-  {
+var WIZARDS_AMOUNT = 4;
+
+// Функция создания массива объектов
+var createWizards = function () {
+  var wizards = {
     name: WIZARD_NAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))] + ' ' + WIZARD_SURNAMES[Math.floor(Math.random() * (WIZARD_SURNAMES.length))],
     coatsColor: COATS_COLOR[Math.floor(Math.random() * (COATS_COLOR.length))],
     eyesColor: EYES_COLOR[Math.floor(Math.random() * (EYES_COLOR.length))]
-  },
-  {
-    name: WIZARD_NAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))] + ' ' + WIZARD_SURNAMES[Math.floor(Math.random() * (WIZARD_SURNAMES.length))],
-    coatsColor: COATS_COLOR[Math.floor(Math.random() * (COATS_COLOR.length))],
-    eyesColor: EYES_COLOR[Math.floor(Math.random() * (EYES_COLOR.length))]
-  },
-  {
-    name: WIZARD_NAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))] + ' ' + WIZARD_SURNAMES[Math.floor(Math.random() * (WIZARD_SURNAMES.length))],
-    coatsColor: COATS_COLOR[Math.floor(Math.random() * (COATS_COLOR.length))],
-    eyesColor: EYES_COLOR[Math.floor(Math.random() * (EYES_COLOR.length))]
-  },
-  {
-    name: WIZARD_NAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))] + ' ' + WIZARD_SURNAMES[Math.floor(Math.random() * (WIZARD_SURNAMES.length))],
-    coatsColor: COATS_COLOR[Math.floor(Math.random() * (COATS_COLOR.length))],
-    eyesColor: EYES_COLOR[Math.floor(Math.random() * (EYES_COLOR.length))]
-  }
-];
+  };
+  return wizards;
+};
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -39,6 +28,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
+// Функция клонирования элементов с разными объектами массива
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -49,10 +39,11 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+// Цикл, запускающий создание массова объектов, клонирование элементов и вставку на страницу
+for (var i = 0; i < WIZARDS_AMOUNT; i++) {
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(renderWizard(createWizards()));
+  similarListElement.appendChild(fragment);
 }
 
-similarListElement.appendChild(fragment);
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
